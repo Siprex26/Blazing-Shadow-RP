1674437d8d69a61359cfe47761e38e7d02909419
Siprex26 <oswaldocacho8@gmail.com>
2025-09-04 05:37:00 -0600

    Create lives.py

---
+++ lives.py
@@ -0,0 +1,7 @@
+from datetime import datetime
+
+# Lista de lives
+lives = [
+    # Ejemplo:
+    # {"nombre": "LiveChill", "fecha": datetime(2025, 9, 4), "confirmado": "si", "estado": "pendiente"}
+]
